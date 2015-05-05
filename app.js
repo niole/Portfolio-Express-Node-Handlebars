12bf2a8dc4f695c64c7881e3abedbf6a3fa166f7
Niole Nelson <niolenelson@gmail.com>
2015-05-05 13:47:23 -0400

    First commit, midway with handlebars templating.

---
+++ app.js
@@ -0,0 +1,84 @@
+"use strict";
+
+/* In case you want to make a .env file*/
+var dotenv = require('dotenv');
+dotenv.load();
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+/* Database (just in case) */
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017", {native_parser:true});
+
+var routes = require('./routes/index');
+/* hooks up a routes file called data.js
+ * just in case you need to store things in a database */
+var data = require('./routes/data');
+
+var app = express();
+
+/* View engine setup. In this case, we're using Handlebars */
+
+app.set('views', path.join(__dirname, 'views'));
+var exphbs = require('express-handlebars');
+app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+/* The following line is commented
+ * out because we do not yet have a database
+ * hooked up */
+//app.use('/data', data);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
